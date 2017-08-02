0d78e65e9176a0ba2b4888161c1b0078458f23c8
blue <bluewang90@gmail.com>
2017-08-02 17:33:54 +0800

    add image

--- WindowsFormsApplication/Form1.Designer.cs
+++ WindowsFormsApplication/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
@@ -42,9 +43,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(13, 395);
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(12, 243);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(138, 44);
+            this.button1.Size = new System.Drawing.Size(214, 44);
             this.button1.TabIndex = 0;
             this.button1.Text = "03 Analysis";
             this.button1.UseVisualStyleBackColor = true;
@@ -52,18 +55,20 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(156, 1);
+            this.pictureBox1.Location = new System.Drawing.Point(232, 1);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(982, 620);
+            this.pictureBox1.Size = new System.Drawing.Size(906, 620);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(13, 264);
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(13, 149);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(137, 41);
+            this.button2.Size = new System.Drawing.Size(214, 44);
             this.button2.TabIndex = 2;
             this.button2.Text = "01 InitSDK";
             this.button2.UseVisualStyleBackColor = true;
@@ -71,9 +76,11 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(13, 311);
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(13, 196);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(137, 41);
+            this.button3.Size = new System.Drawing.Size(214, 44);
             this.button3.TabIndex = 3;
             this.button3.Text = "02 InitBUAnalysis";
             this.button3.UseVisualStyleBackColor = true;
@@ -81,9 +88,11 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(13, 487);
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(13, 293);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(137, 41);
+            this.button4.Size = new System.Drawing.Size(214, 44);
             this.button4.TabIndex = 4;
             this.button4.Text = "04 ReleaseBUAnalysis";
             this.button4.UseVisualStyleBackColor = true;
@@ -91,9 +100,11 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(13, 530);
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.Location = new System.Drawing.Point(12, 340);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(137, 41);
+            this.button5.Size = new System.Drawing.Size(214, 44);
             this.button5.TabIndex = 5;
             this.button5.Text = "05 ReleaseSDK";
             this.button5.UseVisualStyleBackColor = true;
@@ -101,9 +112,11 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(12, 23);
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button6.Location = new System.Drawing.Point(12, 4);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(138, 44);
+            this.button6.Size = new System.Drawing.Size(214, 44);
             this.button6.TabIndex = 6;
             this.button6.Text = "00 Open Image";
             this.button6.UseVisualStyleBackColor = true;
@@ -111,9 +124,11 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(12, 83);
+            this.button7.Image = ((System.Drawing.Image)(resources.GetObject("button7.Image")));
+            this.button7.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button7.Location = new System.Drawing.Point(12, 52);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(138, 44);
+            this.button7.Size = new System.Drawing.Size(214, 44);
             this.button7.TabIndex = 7;
             this.button7.Text = "00 Open Video";
             this.button7.UseVisualStyleBackColor = true;
@@ -121,9 +136,11 @@
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(12, 147);
+            this.button8.Image = ((System.Drawing.Image)(resources.GetObject("button8.Image")));
+            this.button8.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button8.Location = new System.Drawing.Point(12, 100);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(138, 44);
+            this.button8.Size = new System.Drawing.Size(214, 44);
             this.button8.TabIndex = 8;
             this.button8.Text = "00 Open Ok Card Camera";
             this.button8.UseVisualStyleBackColor = true;
@@ -143,8 +160,9 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "乳腺癌b超视频诊断";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
